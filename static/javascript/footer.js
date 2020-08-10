d1ff4e14e3fdb92039ef20a95acf26e72d938763
Gabe <gabezter@gmail.com>
2020-08-10 18:22:57 -0500

    Added login page and move files around

---
+++ static/javascript/footer.js
@@ -0,0 +1,9 @@
+const baseHref = (document.getElementsByTagName('base')[0] || {}).href;
+
+fetch(baseHref + "/partials/footer.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    });
---
+++ static/javascript/header.js
@@ -0,0 +1,9 @@
+const baseHref = (document.getElementsByTagName('base')[0] || {}).href;
+
+fetch(baseHref + "/partials/header.html")
+    .then(response => {
+        return response.text()
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
