ecfd7d266f6ef03e7b64d69e0d9e6b3d47e36524
Gabe <gabezter@gmail.com>
2020-08-10 17:45:52 -0500

    Changed location of base url

--- static/admin/javascript/header-footer.js
+++ static/admin/javascript/header-footer.js
@@ -1,11 +1,13 @@
-fetch("partials/header.html")
+const baseHref = (document.getElementsByTagName('base')[0] || {}).href;
+
+fetch( baseHref + "/partials/header.html")
 .then(response => {
     return response.text()
 })
 .then(data => {
     document.querySelector("header").innerHTML = data;
 });
-fetch("partials/footer.html")
+fetch(baseHref + "/partials/footer.html")
 .then(response => {
     return response.text()
 })
