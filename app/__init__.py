743417eb5cb0b723a27fe573d2be594417a927df
Gabe <gabezter@gmail.com>
2020-08-11 15:40:40 -0500

    Added configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-"""App Init File"""
+"""App Init File."""
 from flask import Flask
 from app.admin import admin_bp
 
 def app():
     app = Flask(__name__)
-
+    app.config.from_pyfile('application.cfg')
     app.register_blueprint(admin_bp)
 
     return app
