736b12e4062379b54946d714a51f91ca2c3d9436
Gabe <gabezter@gmail.com>
2020-08-15 10:57:22 -0500

    Error Catching

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """App Init File."""
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from app.admin import admin_bp
 import app.data_base as db
 
@@ -12,7 +12,7 @@ def app():
 
     @app.route('/init')
     def init():
-        db.init()
+        db.init() 
         return redirect(url_for('admin.Login'))
 
     @app.route('/dump')
@@ -25,4 +25,14 @@ def app():
     @app.route('/login')
     def index():
         return redirect(url_for('admin.Login'))
+
+    def not_found(e):
+        return render_template('not_found.html'), 404
+
+    def internal_error(e):
+        return render_template('internal_error.html'), 500
+
+    app.register_error_handler(404, not_found)
+    app.register_error_handler(500, internal_error)
+    admin_bp.register_error_handler(500, internal_error)
     return app
